063a914d2815c7c0127ea5eca80c7419dcece54d
KhadijeElzein <khady_elzein@hotmail.com>
2020-02-12 00:44:06 -0300

    boilerplate code with configurations set

---
+++ src/main/java/br/com/khadije/upcomingmovieswebapp/UpcomingmovieswebappApplication.java
@@ -0,0 +1,13 @@
+package br.com.khadije.upcomingmovieswebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UpcomingmovieswebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UpcomingmovieswebappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/khadije/upcomingmovieswebapp/UpcomingmovieswebappApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.khadije.upcomingmovieswebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UpcomingmovieswebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
